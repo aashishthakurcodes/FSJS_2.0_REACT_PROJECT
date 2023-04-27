8232c20a07328c6f26d37cbe04b7451fe9e6c061
Aashish Thakur <j9183208@gmail.com>
2023-04-27 22:31:46 +0530

    Project completed

--- REACT_PROJECTS/Game/src/index.js
+++ REACT_PROJECTS/Game/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Appvault from './prc/prc_01';
-// import Main_component from './prc/prc_02';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Appvault />
-    {/* <Main_component/> */}
+  
   </React.StrictMode>
 );
 
