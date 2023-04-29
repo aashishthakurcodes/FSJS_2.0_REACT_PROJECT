d1e2a8cb8fdf3fe9740b39763627ebc0afcda050
Aashish Thakur <j9183208@gmail.com>
2023-04-29 21:59:17 +0530

    Header added

---
+++ REACT_PROJECTS/The_Meal_DB/the_meal_db/src/Header.js/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header=()=>{
+    return(
+        <div>
+         <div className='navbar'>
+            <h2>LoGo</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+         </div>
+        </div>
+    )
+}
+
+export default Header;
