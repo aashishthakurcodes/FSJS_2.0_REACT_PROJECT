f728b29e6c30cfe6b970c6889ce56780d1c7bd8d
Aashish Thakur <j9183208@gmail.com>
2023-05-02 12:40:51 +0530

    Navigation routes

--- REACT_PROJECTS/The_Meal_DB/the_meal_db/src/Header.js/Header.js
+++ REACT_PROJECTS/The_Meal_DB/the_meal_db/src/Header.js/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 const Header=()=>{
     return(
@@ -6,9 +8,12 @@ const Header=()=>{
          <div className='navbar'>
             <h2>LoGo</h2>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+                <Link to={"/"}>
+              <li>Home</li> </Link>
+              <Link to={"/about"}>
+              <li>About</li> </Link>
+              <Link to={"/contact"}>
+              <li>Contact</li> </Link>
             </ul>
             <button>Login</button>
          </div>
--- REACT_PROJECTS/The_Meal_DB/the_meal_db/src/index.js
+++ REACT_PROJECTS/The_Meal_DB/the_meal_db/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter,RouterProvider} from "react-router-dom";
+import { createBrowserRouter,RouterProvider,Outlet} from "react-router-dom";
 import About from "./About_pg/About";
 import Contact from "./Contact_pg/Contact"
 
@@ -23,6 +23,7 @@ const appRouter=createBrowserRouter([
     path:'/contact',
     element:<Contact/>
   },
+  
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
