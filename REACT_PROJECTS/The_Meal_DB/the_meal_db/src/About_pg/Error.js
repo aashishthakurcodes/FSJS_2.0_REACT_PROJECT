7f397949cff5e18aa10ef389f5e786bf90e8602a
Aashish Thakur <j9183208@gmail.com>
2023-05-16 18:25:05 +0530

    Home button

--- REACT_PROJECTS/The_Meal_DB/the_meal_db/src/About_pg/Error.js
+++ REACT_PROJECTS/The_Meal_DB/the_meal_db/src/About_pg/Error.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import Container from '../Container/Container';
+
 
 const Error =()=>{
     return(
         <div>
      <h1>OOPS...No result found</h1>
+
+     <button>
+       <Link to={"/"}>Home</Link>
+     </button>
+    
         </div>
+
+        
     )
 }
 
